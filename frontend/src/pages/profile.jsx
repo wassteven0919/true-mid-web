b0ef6f8bc9ee144199833f26d5ccb5d8469a1928
wassteven <wassteven0919@gmail.com>
2023-04-18 23:14:40 +0800

    change imgur and database and secret

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -55,7 +55,7 @@ function ProfilePage() {
       url: "https://api.imgur.com/3/image/",
       data: formData,
       headers: {
-      Authorization: "Client-ID <type your clientID>",
+      Authorization: "Client-ID 520ffe93b645124",
               Accept: "application/json",
       },
     })
